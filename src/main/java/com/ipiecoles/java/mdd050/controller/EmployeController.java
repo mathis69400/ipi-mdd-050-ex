fa867a2b764258f2810a0acfa38fd7b9320519a9
mathis69400 <mathis.b.69400@gmail.com>
2020-01-03 14:58:38 +0100

    Exo 1

--- src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
@@ -2,7 +2,9 @@ package com.ipiecoles.java.mdd050.controller;
 
 import com.ipiecoles.java.mdd050.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +15,8 @@ public class EmployeController {
     private EmployeRepository employeRepository;
 
     @RequestMapping("/count")
+    //@RequestMapping(value = "/count", method = RequestMethod.GET)
+    //@GetMapping("/count")
     public long countEmployes(){
         //Récupérer le nombre d'employés et l'envoyer au client
         return employeRepository.count();
