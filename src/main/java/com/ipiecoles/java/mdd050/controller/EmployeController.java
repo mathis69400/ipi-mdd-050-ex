c4f9ac8bf3867f4eaef81b8072e2df6abc17f86d
mathis69400 <mathis.b.69400@gmail.com>
2020-01-03 15:53:49 +0100

    Exo 2

--- src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
@@ -1,11 +1,9 @@
 package com.ipiecoles.java.mdd050.controller;
 
+import com.ipiecoles.java.mdd050.model.Employe;
 import com.ipiecoles.java.mdd050.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employes")
@@ -20,5 +18,18 @@ public class EmployeController {
     public long countEmployes(){
         //Récupérer le nombre d'employés et l'envoyer au client
         return employeRepository.count();
+    }
+
+    /*@RequestMapping(value = "/6", method = RequestMethod.GET)
+    public Employe getEmployeById(){
+        //Récupérer le nombre d'employés et l'envoyer au client
+        //return employeRepository.findById(6L).get();
+        return employeRepository.findOne(6L);
+    }*/
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Employe findById(@PathVariable(value = "id") Long id)
+    {
+        return employeRepository.findOne(id);
     }
 }
